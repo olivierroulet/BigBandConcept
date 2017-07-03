73a43104afe0855a27e00be89f217d782a717c6f
PhilNowak <philippenowak@gmail.com>
2017-07-03 18:38:40 +0200

    updated routes.php

--- framework_w/app/routes.php
+++ framework_w/app/routes.php
@@ -1,5 +1,13 @@
 <?php
-	
+//le masque d'URL à associer 
+	// "Default#services",	//le nom du contrôleur et le nom de la méthode à appeler
+			// "default_services"	//le nom de cette route-ci
+
+// LE LIEN : http://localhost/bigband/framework_w/public/artistes_add
+
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+		['GET|POST', '/artistes_add', 'Artistes#addArtiste', 'default_artistes'],
 	);
+
+
