892e2c96e5793f58b97db1d09a1da9445be6d847
Twinpeak <pierre.fr.ancelin@gmail.com>
2017-07-05 15:34:31 +0200

    je ne sais pas quoi commiter

--- framework_w/app/Views/public_layout.php
+++ framework_w/app/Views/public_layout.php
@@ -41,7 +41,7 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
 						<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> 
 					</button> 
-					<a class="navbar-brand" href="#home"><h1><img src="images/portfolio/portfolio1.jpg" alt="logo">BIG BAND</h1></a> 
+					<a class="navbar-brand" href="#home"><h1><!--<img src="images/logo.png" alt="logo">-->BIG BAND</h1></a> 
 				</div> 
 				<div class="collapse navbar-collapse"> 
 					<ul class="nav navbar-nav navbar-right"> 
