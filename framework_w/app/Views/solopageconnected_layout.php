b87aadd50747e6f43d3ae16fe9deb9d1d071acb9
Twinpeak <pierre.fr.ancelin@gmail.com>
2017-07-11 19:20:06 +0200

    css more

--- framework_w/app/Views/solopageconnected_layout.php
+++ framework_w/app/Views/solopageconnected_layout.php
@@ -53,8 +53,7 @@
 				</div> 
 				<div class="pull-right"><a href="logout" class="btn btn-danger" id="buttondecadmin">Se déconnecter</a>
 				</div> 
-				<div class="pull-right"><a href="artiste_liste_PDF.php" class="btn btn-info" id="buttonimpradmin">Imprimer en PDF</a>	
-				</div> 
+				 
 			</div><!--/navbar--> 
         </div>
 		</header> <!--/#navigation-->
@@ -112,8 +111,7 @@
                     </div> 
                     <div class="pull-right"><a href="logout" class="btn btn-danger" id="buttondecadmin">Se déconnecter</a>
                     </div> 
-                    <div class="pull-right"><a href="artiste_liste_PDF.php" class="btn btn-info" id="buttonimpradmin">Imprimer en PDF</a>	
-                    </div> 
+                    
                 </div><!--/navbar--> 
             </div>
         </header> <!--/#navigation-->
--- framework_w/app/Views/views_admin/info_groupe.php
+++ framework_w/app/Views/views_admin/info_groupe.php
@@ -9,98 +9,98 @@
             <div class="row text-center">
                 <div class="col-sm-1.5 pull-left">
                     <a href="redirect_role" class="btn btn-primary">Menu principal</a>
-                    <a href="update_info_groupe" class="btn btn-warning">Update</a>
+                    <a href="update_info_groupe" class="btn btn-warning" id="btn-color-update" title="retour">Update</a>
                 </div>
             </div>
        <br>
-            <div class="row">
+            <div class="row" >
 
             <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
+                <div class="panel panel-primary" >
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Nom du Groupe</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_Name']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Photo</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_PhotoMainPagePath']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Logo</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                        <?php echo $group['GI_Logo']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Adresse</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_Address']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Code Postal</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_Addr_ZipCode']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Ville</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_Addr_City']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Heure d'ouverture</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_OpeningHours']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">Commentaire</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_Comments']?>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="panel-heading-color">
                         <h3 class="panel-title">News</h3>
                     </div>
-                    <div class="panel-body">
+                    <div class="panel-body" id="panel-body-color">
                         <?php echo $group['GI_News']?>
                     </div>
                 </div>
