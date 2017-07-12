6f361d4d1db70b244d67a5d94f153cfbcb05d358
meandmyself33 <mollet.geoffrey@gmail.com>
2017-07-12 14:33:25 +0200

    liste employeur

--- framework_w/app/Model/ClientsModel.php
+++ framework_w/app/Model/ClientsModel.php
@@ -9,34 +9,34 @@ namespace Model;
 class ClientsModel extends \W\Model\Model // Attention à l' arborescence !!!
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->setPrimaryKey('CL_Idclient'); // cette variable permet de ne pas être coincé par w\Model.php 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setPrimaryKey('CL_Idclient'); // cette variable permet de ne pas être coincé par w\Model.php 
 
-	}
+    }
 
-/**
+    /**
 	 * Récupère une ligne de la table en fonction d'un identifiant
 	 * @param  integer Identifiant
 	 * @return mixed Les données sous forme de tableau associatif
 	 */
-	public function findClientByIdUser($idUser)
-	{
-		if (!is_numeric($idUser)){
-			return false;
-		}
+    public function findClientByIdUser($idUser)
+    {
+        if (!is_numeric($idUser)){
+            return false;
+        }
 
-		$sql = 'SELECT * FROM ' . $this->table . ' WHERE CL_ID_InUsersTable = :idUser LIMIT 1';
-		$sth = $this->dbh->prepare($sql);
-		$sth->bindValue(':idUser', $idUser);
-		$sth->execute();
+        $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_ID_InUsersTable = :idUser LIMIT 1 ';
+        $sth = $this->dbh->prepare($sql);
+        $sth->bindValue(':idUser', $idUser);
+        $sth->execute();
 
-		return $sth->fetch();
-	}
+        return $sth->fetch();
+    }
 
 
-/**
+    /**
 	 * Récupère toutes les lignes de la table employeur, liée à la table user
 	 * @param $orderBy La colonne en fonction de laquelle trier
 	 * @param $orderDir La direction du tri, ASC ou DESC
@@ -44,43 +44,95 @@ class ClientsModel extends \W\Model\Model // Attention à l' arborescence !!!
 	 * @param $offset La position à partir de laquelle récupérer les résultats
 	 * @return array Les données sous forme de tableau multidimensionnel
 	 */
-	public function findAllEmployeurs($orderBy = "", $orderDir = 'ASC', $limit = null, $offset = null)
-	{
-		$sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id ';
-		
-		
-		if (!empty($orderBy)){
+    public function findAllEmployeurs($orderBy = "", $orderDir = 'ASC', $limit = null, $offset = null)
+    {
+        $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id ';
 
-			if(!preg_match('#^[a-zA-Z0-9_$]+$#', $orderBy)){
-				die('Error: invalid orderBy param');
-			}
-			$orderDir = strtoupper($orderDir);
-			if($orderDir != 'ASC' && $orderDir != 'DESC'){
-				die('Error: invalid orderDir param');
-			}
-			if ($limit && !is_int($limit)){
-				die('Error: invalid limit param');
-			}
 
-			if ($offset && !is_int($offset)){
-				die('Error: invalid offset param');
-			}
+        if (!empty($orderBy)){
 
-			if($orderBy!=''){
-				$sql .= ' ORDER BY '.$orderBy.' '.$orderDir;
-			}
-		}
-/*		if($limit){
-			$sql .= ' LIMIT '.$limit;
-			if($offset){
-				$sql .= ' OFFSET '.$offset;
-			}
-		}*/
-		$sth = $this->dbh->prepare($sql);
-		$sth->execute();
-		return $sth->fetchAll();
-	}
+            if(!preg_match('#^[a-zA-Z0-9_$]+$#', $orderBy)){
+                die('Error: invalid orderBy param');
+            }
+            $orderDir = strtoupper($orderDir);
+            if($orderDir != 'ASC' && $orderDir != 'DESC'){
+                die('Error: invalid orderDir param');
+            }
+            if ($limit && !is_int($limit)){
+                die('Error: invalid limit param');
+            }
+
+            if ($offset && !is_int($offset)){
+                die('Error: invalid offset param');
+            }
+
+            if($orderBy!=''){
+                $sql .= ' ORDER BY '.$orderBy.' '.$orderDir;
+            }
+        }
+        if($limit){
+            $sql .= ' LIMIT '.$limit;
+            if($offset){
+                $sql .= ' OFFSET '.$offset;
+            }
+        }
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+
+
+
+    public function findNextEmployeur($id){
+        if (!is_numeric($id)){
+            return false;
+        }
+        /*
+$sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_Idclient  = (SELECT MIN(CL_Idclient) FROM ' . $this->table . ' WHERE CL_Idclient > :idActuel) LIMIT 1';
+        */
+
+        $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_Idclient  = (SELECT MIN(CL_Idclient) FROM ' . $this->table . ' WHERE CL_Idclient > :idActuel) LIMIT 1';
+        $sth = $this->dbh->prepare($sql);
+        $sth->bindValue(':idActuel', $id);
+        $sth->execute();
+
+        $result = $sth->fetch();
+        
+        if(!$result) {
+            $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_Idclient  = (SELECT MIN(CL_Idclient) FROM ' . $this->table . ')  LIMIT 1';
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute();
+            $result = $sth->fetch();
+        }
+
+        return $result;
+    }
+    
+    
+    
+    
+    public function findPreviousEmployeur($id){
+        if (!is_numeric($id)){
+            return false;
+        }
+
+         $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_Idclient  = (SELECT MIN(CL_Idclient) FROM ' . $this->table . ' WHERE CL_Idclient < :idActuel) LIMIT 1';
+        $sth = $this->dbh->prepare($sql);
+        $sth->bindValue(':idActuel', $id);
+        $sth->execute();
+
+        $result = $sth->fetch();
+        $sql = 'SELECT * FROM ' . $this->table . ' INNER JOIN users on CL_ID_InUsersTable=US_Id WHERE CL_Idclient  = (SELECT MIN(CL_Idclient) FROM ' . $this->table . ') LIMIT 1';
+        $sth = $this->dbh->prepare($sql);
+        $sth->bindValue(':idActuel', $id);
+        $sth->execute();
+
+        return $result;
+    }
+
 }
+
 
 
 //`CL_Idclient`, `CL_Prenom`, `CL_Nom`, `CL_Telephone`, `CL_Raison_Sociale`, `CL_Statut_Juridique`, `CL_Titulaire_Licence_Entrepreneur_De_Spectacles`, `CL_Code_Postal`, `CL_Ville`, `CL_Habitude_De_Date`, `CL_Date_De_Prise_De_Decision`, `CL_Date_Du_Prochain_Suivi`, `CL_Type_Bareme`, `CL_Indicateur_De_Suivi`, `CL_Statut_Du_Client`
--- framework_w/app/Views/views_employeur/liste_des_employeurs.php
+++ framework_w/app/Views/views_employeur/liste_des_employeurs.php
@@ -3,177 +3,182 @@
 <?php $this->start('main_content') ?>
 
 <section class="parallax-section">
-    <div class="container-fluid">
+    <div class="container">
         <div class="clients-wrapper">
             <div class="row text-center">
                 <div class="col-sm-1.5 pull-left">
                     <a href="redirect_role" class="btn btn-primary">Menu principal</a>
                 </div>
             </div>
-            <?php debug($employeurs); ?>
+
             <div class="row text-center">
-                <div class="col-sm-6 col-sm-offset-3">
-                    <h2 class="title-one">Gestion des employeurs</h2>
-                    <div class="row">
-                        <?php 
-                        if(!empty($employeurs)):?>
-                        <hr>
-                        <?php foreach ($employeurs as $employeur): 
 
-                        switch ($employeur['US_idURole']) {
-                            case 1:
-                                $role='Administrateur';
-                                break;
-                            case 2:
-                                $role='Employeur';
-                                break;
-                            case 3:
-                                $role='Artiste';
-                                break;
-                            case 4:
-                                $role='Fan';
-                                break;
-                        }?>
+                <h2 class="title-one">Gestion des employeurs</h2>
 
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Prenom</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['US_FirstName'];?>               </div>
-                            </div>
+                
+    <?php debug($employeurs);
+    foreach($employeurs as $employeur){
+        if(!empty($employeur)):?>
+                <hr>
+
+
+                <form method="POST" action="employeursuivant">
+                   <button type="submit" class="btn btn-primary">Suivant</button>
+                    <input name="actualIdClient" type="hidden" value="<?=$employeur['CL_Idclient'];?>">
+                </form>
+                    <form method="POST" action="employeurprecedent">
+                    <input name="actualIdClient" type="hidden" value="<?=$employeur['CL_Idclient'];?>">
+                    <button  type="submit" class="btn btn-primary">Precedent</button>
+                </form>
+                <br>
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Prenom</h3>
                         </div>
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Nom</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['US_LastName'];?>               </div>
-                            </div>
-                        </div>                       
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Email</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['US_email'];?>               </div>
-                            </div>
-                        </div>                  
-
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Téléphone</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['US_tel'];?>               </div>
-                            </div>
+                        <div class="panel-body">
+                            <?=$employeur['US_FirstName'];?>
                         </div>
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Raison Sociale</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Raison_Sociale'];?> </div>
-                            </div>
-                        </div>                       
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Statut Juridique</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Statut_Juridique'];?>
-                                </div>
-                            </div>
-                        </div>                       
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Titulaire Licence Entrepreneur De Spectacles</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Titulaire_Licence_Entrepreneur_De_Spectacles'];?>
-                                </div>
-                            </div>
-                        </div>                      
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Code postal/Ville</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Code_Postal'];?><?=$employeur['CL_Ville'];?>
-                                </div>
-                            </div>
-                        </div>                      
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Habitude de date</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Habitude_De_Date'];?>
-                                </div>
-                            </div>
-                        </div>                             
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Date du prochain suivi</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Date_Du_Prochain_Suivi'];?>
-                                </div>
-                            </div>
-                        </div>                           
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Bareme</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Type_Bareme'];?>
-                                </div>
-                            </div>
-                        </div>                          
-                        <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Indicateur du suivi</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Indicateur_De_Suivi'];?>
-                                </div>
-                            </div>
-                        </div>                        
-                           <div class="col-sm-4">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Statut du client</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <?=$employeur['CL_Statut_Du_Client'];?>
-                                </div>
-                            </div>
-                        </div>
-
-
                     </div>
-
-                    <hr>
-
-                    <?php endforeach; ?>
-                    <?php else: ?>
-                    <div class="alert alert-danger">
-                        Aucun utilisateur !
-                    </div>
-                    <?php endif; ?>
                 </div>
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Nom</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['US_LastName'];?>
+                        </div>
+                    </div>
+                </div>                       
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Email</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['US_email'];?>               
+                        </div>
+                    </div>
+                </div>                  
+
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Téléphone</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['US_tel'];?>               
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Raison Sociale</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Raison_Sociale'];?> 
+                        </div>
+                    </div>
+                </div>                       
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Statut Juridique</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Statut_Juridique'];?>
+                        </div>
+                    </div>
+                </div>                       
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Titulaire Licence Entrepreneur De Spectacles</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Titulaire_Licence_Entrepreneur_De_Spectacles'];?>
+                        </div>
+                    </div>
+                </div>                      
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Code postal/Ville</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Code_Postal'];?><?=$employeur['CL_Ville'];?>
+                        </div>
+                    </div>
+                </div>                      
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Habitude de date</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Habitude_De_Date'];?>
+                        </div>
+                    </div>
+                </div>                             
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Date du prochain suivi</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Date_Du_Prochain_Suivi'];?>
+                        </div>
+                    </div>
+                </div>                           
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Bareme</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Type_Bareme'];?>
+                        </div>
+                    </div>
+                </div>                          
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Indicateur du suivi</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Indicateur_De_Suivi'];?>
+                        </div>
+                    </div>
+                </div>                        
+                <div class="col-sm-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Statut du client</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?=$employeur['CL_Statut_Du_Client'];?>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <hr>
+
+
+
+                <?php else: ?>
+                <div class="alert alert-danger">
+                    Aucun utilisateur !
+                </div>
+                <?php endif;         ?>
+                <?php
+    }
+                ?>
             </div>
         </div>
     </div>
